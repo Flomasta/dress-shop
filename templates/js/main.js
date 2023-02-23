74c4b13823d0f6916b794295b49e8895f9f224f7
flomasta <zatevi@mail.ru>
2023-02-23 15:48:12 +0300

    - finished detail page

--- templates/js/main.js
+++ templates/js/main.js
@@ -1,6 +1,8 @@
 const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
 const close = document.getElementById('close');
+const MainImg = document.getElementById('MainImg');
+const small_img = document.getElementsByClassName('small-img');
 
 if (bar) {
     bar.addEventListener('click', () => {
@@ -12,4 +14,21 @@ if (close) {
     close.addEventListener('click', () => {
         nav.classList.remove('active');
     })
+}
+
+small_img[0].onclick= function (){
+    MainImg.src = small_img[0].src;
+
+}
+small_img[1].onclick= function (){
+    MainImg.src = small_img[1].src;
+
+}
+small_img[2].onclick= function (){
+    MainImg.src = small_img[2].src;
+
+}
+small_img[3].onclick= function (){
+    MainImg.src = small_img[3].src;
+
 }
